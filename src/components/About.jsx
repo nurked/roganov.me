05cf4977672e852f31b10e2aa1b40adc9448b6c0
Atonu Shaha <atonushaha@Atonus-MacBook-Air.local>
2025-01-22 12:35:13 +0600

    fixing the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export const About = () => {
     return (
-        <div id="about" className=" bg-[#96C9F4] w-full h-auto md:h-screen flex flex-col md:flex-row">
+        <div id="about" className=" bg-[#fafbfb] w-full h-auto md:h-screen flex flex-col md:flex-row">
             {/* Left section (Image and Name) */}
-            <div className="w-full md:w-1/3  relative flex flex-col items-center justify-center p-4">
+            <div className="w-full md:w-1/3 relative flex flex-col items-center justify-center ">
                 <img
                     src="https://images.pexels.com/photos/1105379/pexels-photo-1105379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
                     alt="Placeholder"
-                    className="w-full h-1/2 max-w-[calc(100%-2rem)] rounded-lg object-cover"
+                    className="w-2/3 h-2/3 mt-10 max-w-2/3 rounded-lg object-cover"
                 />
                 <div className="mt-4 text-center text-black">
                     <p className="font-bold text-lg">Ivan Roganov</p>
@@ -17,9 +17,9 @@ export const About = () => {
             </div>
 
             {/* Right section (Content) */}
-            <div className="w-full md:w-2/3  p-8 flex flex-col items-center justify-center">
-                <div className="w-full max-w-3xl">
-                    <p className="mb-4 text-lg font-bold">
+            <div className="w-full flex flex-col items-center justify-center">
+                <div className="w-3/4 md:w-3/4 max-w-3xl">
+                    <p className=" text-lg font-bold">
                     With 25 years of experience in IT, I’ve been programming since 1999, honing my skills and expertise across multiple areas of technology. I graduated from Moscow State University of Informatics in 2008 with a degree in Computer Science, where my foundation in IT was solidified.
                     </p>
                     <p className="mb-4 text-lg font-bold">
@@ -34,7 +34,7 @@ export const About = () => {
                     <p className="mb-4 text-lg font-bold">
                         There isn’t a program that can’t be fixed, and there isn’t an IT system that can’t be made operational.
                     </p>
-                    <p className="mb-4 text-lg font-bold">
+                    <p className="mb-10 text-lg font-bold">
                     In my career, I’ve learned that no program is beyond repair, and no IT system is beyond restoration. There’s always a way to fix it and make it better.
                     </p>
                 </div>
